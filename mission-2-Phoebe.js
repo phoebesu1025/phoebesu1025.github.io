def14c51149c84a7a94cffa70ec5a9556f62dafb
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-09-10 18:17:08 +1200

    adding new page and photos buttons

--- mission-2-Phoebe.js
+++ mission-2-Phoebe.js
@@ -1,16 +1,16 @@
 const aboutContainer = document.getElementById('about-container')
-aboutContainer.addEventListener('mouseover',function(){
+aboutContainer.addEventListener('mouseover', function () {
     console.log("what")
 })
 
-
+//use function within html & function eventlistener
 /*for contactme*/
-const alertFunc = () =>{
-   return alert("Thank you for your equery, I'll get back to you soon.")
+const alertFunc = () => {
+    return alert("Thank you for your equery, I'll get back to you soon.")
 }
 /*----------------------------*/
 
 const signUpBtn = document.querySelector(".signUpBtn");
-signUpBtn.addEventListener('click', function() {
-   return alert("Thank you for Signing up, I'll update more photos!")  
+signUpBtn.addEventListener('click', function () {
+    return alert("Thank you for Signing up, I'll update more photos!")
 });
