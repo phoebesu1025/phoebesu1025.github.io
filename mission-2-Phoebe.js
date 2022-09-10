6f60a09146922454266dcf206484c7ea92c7ea83
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-09-10 12:50:59 +1200

    style pages, creating some js function

--- mission-2-Phoebe.js
+++ mission-2-Phoebe.js
@@ -2,3 +2,15 @@ const aboutContainer = document.getElementById('about-container')
 aboutContainer.addEventListener('mouseover',function(){
     console.log("what")
 })
+
+
+/*for contactme*/
+const alertFunc = () =>{
+   return alert("Thank you for your equery, I'll get back to you soon.")
+}
+/*----------------------------*/
+
+const signUpBtn = document.querySelector(".signUpBtn");
+signUpBtn.addEventListener('click', function() {
+   return alert("Thank you for Signing up, I'll update more photos!")  
+});
